278ef1cfea13382cfcc58e9ef4ea2b90a279d875
Jennifer Olenchak <59713234+jenniferolenchak@users.noreply.github.com>
2023-02-09 00:22:10 -0500

    Combine .gitignore files & relocate files from redundant 'Bin' folder

---
+++ Application/application.py
@@ -0,0 +1,87 @@
+# This Python file uses the following encoding: utf-8
+from pathlib import Path
+import sys
+import os
+
+from PySide6.QtWidgets import QApplication, QStackedWidget
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import QThreadPool
+from LoadScreen import LoadScreen
+
+def ImportGlobalModules(loadScreen):
+    loadScreen.updateLoadStatus(percentComplete=5, statusText="Importing AirSim")
+    global airsim
+    import airsim
+
+    loadScreen.updateLoadStatus(percentComplete=15, statusText="Importing LaunchScreen")
+    global LaunchScreen
+    from LaunchScreen import LaunchScreen
+
+    loadScreen.updateLoadStatus(percentComplete=20, statusText="Importing VQAInteractionScreen")
+    global VQAInteractionScreen
+    from VQAInteractionScreen import VQAInteractionScreen
+
+    loadScreen.updateLoadStatus(percentComplete=30, statusText="Importing AirSimControl")
+    global AirSimControl
+    from AirSimControl import AirSimControl
+
+    loadScreen.updateLoadStatus(percentComplete=40, statusText="Importing setupViltTransformer")
+    global setupViltTransformer
+    from utils import setupViltTransformer
+
+    loadScreen.updateLoadStatus(percentComplete=50, statusText="Importing setupLxmertTransformer")
+    global setupLxmertTransformer
+    from utils import setupLxmertTransformer
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    stackedWidget = QStackedWidget()
+    stackedWidget.resize(500, 500)
+    stackedWidget.setWindowTitle("DroneVQA")
+    stackedWidget.setWindowIcon(QIcon("Images/Logos/logo_drone_only.png"))
+
+
+    # Display the load screen until the initialization processes are done
+    loadScreen = LoadScreen(stackedWidget)
+    stackedWidget.addWidget(loadScreen)
+    stackedWidget.setCurrentWidget(loadScreen)
+    stackedWidget.resize(500,500)
+    stackedWidget.show()
+
+    # Import modules while the loading screen  is displaying
+    ImportGlobalModules(loadScreen)
+
+    loadScreen.updateLoadStatus(percentComplete=55, statusText="Initializing AirSimControl")
+    controller = AirSimControl()
+
+    loadScreen.updateLoadStatus(percentComplete=60, statusText="Initializing QThreadPool")
+    threadManager = QThreadPool()
+
+    # Initialize global variables
+    loadScreen.updateLoadStatus(percentComplete=65, statusText="Initializing global camera variables")
+    CAMERA_NAME = '0'
+    IMAGE_TYPE = airsim.ImageType.Scene
+    DECODE_EXTENSION = '.png'
+    record = True
+
+    # Setup Models 
+    loadScreen.updateLoadStatus(percentComplete=65, statusText="Beginning to set up models...")
+    models = []
+    loadScreen.updateLoadStatus(percentComplete=60, statusText="Initializing Vilt model\nThis step will take longer the first time this application loads.")
+    models.append((setupViltTransformer()))
+    loadScreen.updateLoadStatus(percentComplete=80, statusText="Initializing LxMERT model\nThis step will take longer the first time this application loads.")
+    models.append((setupLxmertTransformer()))
+    loadScreen.updateLoadStatus(percentComplete=95, statusText="Switching to launch screen...")
+
+    # Switch to the launch screen
+    VQAScreen = VQAInteractionScreen(threadManager, controller, models)
+    launchScreen = LaunchScreen(stackedWidget, threadManager, VQAScreen, controller)
+    stackedWidget.addWidget(launchScreen)
+    stackedWidget.addWidget(VQAScreen)
+    stackedWidget.resize(500, 625)
+    stackedWidget.setCurrentWidget(launchScreen)
+
+    sys.exit(app.exec())
+
---
+++ Application/ui_LaunchScreen.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'LaunchScreen.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QPushButton,
+    QSizePolicy, QVBoxLayout, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(502, 481)
+        self.gridLayoutWidget = QWidget(Form)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(40, 300, 421, 101))
+        self.gridLayout_SelectVirtalEnvArea = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_SelectVirtalEnvArea.setObjectName(u"gridLayout_SelectVirtalEnvArea")
+        self.gridLayout_SelectVirtalEnvArea.setContentsMargins(0, 0, 0, 0)
+        self.button_AlreadyLaunched = QPushButton(self.gridLayoutWidget)
+        self.button_AlreadyLaunched.setObjectName(u"button_AlreadyLaunched")
+
+        self.gridLayout_SelectVirtalEnvArea.addWidget(self.button_AlreadyLaunched, 2, 0, 1, 1)
+
+        self.label_VirtualEnvSelectPrompt = QLabel(self.gridLayoutWidget)
+        self.label_VirtualEnvSelectPrompt.setObjectName(u"label_VirtualEnvSelectPrompt")
+        self.label_VirtualEnvSelectPrompt.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_SelectVirtalEnvArea.addWidget(self.label_VirtualEnvSelectPrompt, 0, 0, 1, 1)
+
+        self.gridLayout_EnvLaunchButtons = QGridLayout()
+        self.gridLayout_EnvLaunchButtons.setObjectName(u"gridLayout_EnvLaunchButtons")
+        self.button_LaunchTestMap = QPushButton(self.gridLayoutWidget)
+        self.button_LaunchTestMap.setObjectName(u"button_LaunchTestMap")
+
+        self.gridLayout_EnvLaunchButtons.addWidget(self.button_LaunchTestMap, 0, 1, 1, 1)
+
+        self.button_LaunchCityMap = QPushButton(self.gridLayoutWidget)
+        self.button_LaunchCityMap.setObjectName(u"button_LaunchCityMap")
+
+        self.gridLayout_EnvLaunchButtons.addWidget(self.button_LaunchCityMap, 0, 0, 1, 1)
+
+
+        self.gridLayout_SelectVirtalEnvArea.addLayout(self.gridLayout_EnvLaunchButtons, 1, 0, 1, 1)
+
+        self.label_LaunchScreenTitle = QLabel(Form)
+        self.label_LaunchScreenTitle.setObjectName(u"label_LaunchScreenTitle")
+        self.label_LaunchScreenTitle.setGeometry(QRect(150, 0, 191, 91))
+        self.label_LaunchScreenDescription = QLabel(Form)
+        self.label_LaunchScreenDescription.setObjectName(u"label_LaunchScreenDescription")
+        self.label_LaunchScreenDescription.setGeometry(QRect(20, 80, 471, 61))
+        self.label_LaunchScreenDescription.setAlignment(Qt.AlignCenter)
+        self.label_LaunchScreenDescription.setWordWrap(True)
+        self.verticalLayoutWidget = QWidget(Form)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(80, 420, 331, 48))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label_AirSimLaunchInstruction = QLabel(self.verticalLayoutWidget)
+        self.label_AirSimLaunchInstruction.setObjectName(u"label_AirSimLaunchInstruction")
+
+        self.verticalLayout.addWidget(self.label_AirSimLaunchInstruction)
+
+        self.button_InitializeClient = QPushButton(self.verticalLayoutWidget)
+        self.button_InitializeClient.setObjectName(u"button_InitializeClient")
+
+        self.verticalLayout.addWidget(self.button_InitializeClient)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.button_AlreadyLaunched.setText(QCoreApplication.translate("Form", u"[I have already launched AirSim myself]", None))
+        self.label_VirtualEnvSelectPrompt.setText(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" font-size:12pt; font-weight:700;\">Select A Virtual  Environment To Explore:</span></p></body></html>", None))
+        self.button_LaunchTestMap.setText(QCoreApplication.translate("Form", u"Launch Test Map", None))
+        self.button_LaunchCityMap.setText(QCoreApplication.translate("Form", u"Launch City Map", None))
+        self.label_LaunchScreenTitle.setText(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" font-size:28pt; font-weight:700; color:#000000;\">DroneVQA</span></p></body></html>", None))
+        self.label_LaunchScreenDescription.setText(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" font-size:16pt; color:#1315a6;\">Utilizing Artifical Intelligence For Visual Question Anwering With Simulated Drones</span></p></body></html>", None))
+        self.label_AirSimLaunchInstruction.setText(QCoreApplication.translate("Form", u"<html><head/><body><p><span style=\" color:#740000;\">Click </span><span style=\" font-style:italic; color:#740000;\">&quot;No&quot;</span><span style=\" color:#740000;\"> in AirSim to select quadrotor drone , then click here:</span></p></body></html>", None))
+        self.button_InitializeClient.setText(QCoreApplication.translate("Form", u"Initialize Airsim VQA Client", None))
+    # retranslateUi
+
