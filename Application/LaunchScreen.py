7dad527743678c158bdbc2156de85b9d92fe9a8d
Jennifer Olenchak <59713234+jenniferolenchak@users.noreply.github.com>
2023-02-09 01:53:23 -0500

    Update Launch Screen & Relocate Loading .gif

--- Application/LaunchScreen.py
+++ Application/LaunchScreen.py
@@ -28,14 +28,8 @@ class LaunchScreen(QWidget):
         ui_file.close()
         self.resize(500, 625)
 
-        # Connect button actions to methods
-        self.ui.button_AlreadyLaunched.clicked.connect(self.showFurtherInstructions)
+        # Connect button action to method
         self.ui.button_InitializeClient.clicked.connect(self.startVQA)
-        self.ui.button_LaunchCityMap.clicked.connect(lambda: self.launchAirSimEnv("..\\Environments\\CityEnvironment\\CityEnviron.exe"))
-
-        # Initially hide further user instructions
-        self.ui.button_InitializeClient.hide()
-        self.ui.label_AirSimLaunchInstruction.hide()
 
     def launchAirSimEnv(self, relativePath):
         '''Launch AirSim environment executable and display further instructions to user'''
@@ -43,11 +37,6 @@ class LaunchScreen(QWidget):
         mapPath = os.path.join(projectPath, relativePath)
         self.runningSimulation = os.startfile(mapPath)
         self.showFurtherInstructions()
-
-    def showFurtherInstructions(self):
-        '''Display VQA initilaization button and instruction to click button'''
-        self.ui.label_AirSimLaunchInstruction.show()
-        self.ui.button_InitializeClient.show()
 
     def navToVQAScreen(self):
         '''Initialize VQA interaction screen and set at the active stacked frame'''
